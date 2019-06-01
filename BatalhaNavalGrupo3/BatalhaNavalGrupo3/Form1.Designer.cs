2f69646d35e7b085ed1499635a97ae91a1c69b1d
Oscar L. H. Henrichs <oscar.henrichs.work@outlook.com>
2019-06-01 03:20:43 -0300

    Projeto Inicial

---
+++ BatalhaNavalGrupo3/BatalhaNavalGrupo3/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace BatalhaNavalGrupo3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(42, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(353, 371);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ BatalhaNavalGrupo3/BatalhaNavalGrupo3/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BatalhaNavalGrupo3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int RowCount = 10;
+        const int ColumnCount = 10;
+        Label[,] quadradoGlobal = new Label[10, 10];
+        Boolean[,] quadradoMarcado = new Boolean[10, 10];
+
+
+        private void selecionar_navios(object sender, System.EventArgs e)
+        {
+            Label lbl = (Label)sender;
+            Color color = lbl.BackColor;
+            Point local = lbl.Location;
+
+            if (color == System.Drawing.Color.White)
+            {
+                color = System.Drawing.Color.Black;
+                quadradoMarcado[lbl.Location.X / 20, lbl.Location.Y / 20] = true;
+                Console.WriteLine(quadradoMarcado[lbl.Location.X / 20, lbl.Location.Y / 20]);
+            }
+            else if (color == System.Drawing.Color.Black)
+            {
+                color = System.Drawing.Color.Red;               
+                quadradoMarcado[lbl.Location.X / 20, lbl.Location.Y / 20] = true;
+                Console.WriteLine(quadradoMarcado[lbl.Location.X / 20, lbl.Location.Y / 20]);
+            }
+            else
+            {
+                color = System.Drawing.Color.White;
+                quadradoMarcado[lbl.Location.X / 20, lbl.Location.Y / 20] = false;
+                Console.WriteLine(quadradoMarcado[lbl.Location.X / 20, lbl.Location.Y / 20]);
+            }
+            lbl.BackColor = color;
+            Console.WriteLine(lbl.Location);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < RowCount; i++)
+            {
+                for (int j = 0; j < ColumnCount; j++)
+                {
+                    quadradoGlobal[i, j] = new Label();
+                    quadradoGlobal[i, j].Name = $"({i},{j})";
+                    quadradoGlobal[i, j].Size = new Size(20, 20);
+                    quadradoGlobal[i, j].Location = new Point(i * 20, j * 20);
+                    quadradoGlobal[i, j].BackColor = Color.White;
+                    quadradoGlobal[i, j].Click += selecionar_navios;
+                    quadradoMarcado[i, j] = false;
+                    panel1.Controls.Add(quadradoGlobal[i, j]);
+                }
+            }
+        }
+    }
+}
