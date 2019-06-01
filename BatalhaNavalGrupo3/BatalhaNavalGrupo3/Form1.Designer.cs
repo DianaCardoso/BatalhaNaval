21920489dd4799fe3eb7e18d80593b37210f712c
Oscar H. L. Henrichs <oscar.henrichs.work@outlook.com>
2019-06-01 05:03:32 -0300

    Adicionei botão de start e os botoes com o valor de cada navio

--- BatalhaNavalGrupo3/BatalhaNavalGrupo3/Form1.Designer.cs
+++ BatalhaNavalGrupo3/BatalhaNavalGrupo3/Form1.Designer.cs
@@ -28,23 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.PainelJogo = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
+            // PainelJogo
+            // 
+            this.PainelJogo.Location = new System.Drawing.Point(345, 12);
+            this.PainelJogo.Name = "PainelJogo";
+            this.PainelJogo.Size = new System.Drawing.Size(455, 446);
+            this.PainelJogo.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Começar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Botao_Start);
+            // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(42, 33);
+            this.panel1.Location = new System.Drawing.Point(22, 59);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(353, 371);
-            this.panel1.TabIndex = 0;
+            this.panel1.Size = new System.Drawing.Size(261, 368);
+            this.panel1.TabIndex = 2;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(812, 491);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PainelJogo);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -53,6 +74,8 @@
 
         #endregion
 
+        private System.Windows.Forms.Panel PainelJogo;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel panel1;
     }
 }
--- BatalhaNavalGrupo3/BatalhaNavalGrupo3/Form1.cs
+++ BatalhaNavalGrupo3/BatalhaNavalGrupo3/Form1.cs
@@ -12,40 +12,88 @@ namespace BatalhaNavalGrupo3
 {
     public partial class Form1 : Form
     {
+        //Variáveis Iniciais
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        // Numero de Colunas e linhas
         const int RowCount = 10;
         const int ColumnCount = 10;
-        Label[,] quadradoGlobal = new Label[10, 10];
-        Boolean[,] quadradoMarcado = new Boolean[10, 10];
+        Boolean definindo_jogo = false;
+        Boolean jogando_jogo = false;
 
 
-        private void selecionar_navios(object sender, System.EventArgs e)
+        //Valores Armazenados
+        Label[,] quadradoGlobal = new Label[10, 10]; // Armazenei cada label gerada dentro de uma matriz.
+        bool[,] quadradoMarcado = new bool[10, 10]; // Um Boolean pra salvar os quadrados marcados.
+        Button[] BotoesNavios = new Button[5];
+        string[] QuantidadeNavios = new string[5] { "5", "4", "3", "3", "2" };
+
+        private void Botao_Start(object sender, EventArgs e)
         {
-            Label lbl = (Label)sender;
-            Color color = lbl.BackColor;
+            definindo_jogo = true;
+
+            if (definindo_jogo == true)
+            {
+                for (int i = 0; i < RowCount; i++)// Esses Laços for geram a tabela de labels que mudam de cor.
+                {
+                    for (int j = 0; j < ColumnCount; j++)
+                    {
+                        quadradoGlobal[i, j] = new Label
+                        {
+                            Name = $"({i},{j})",
+                            Size = new Size(30, 30),
+                            Location = new Point(i * 30, j * 30),
+                            BackColor = Color.White
+                        };
+                        quadradoGlobal[i, j].Click += Selecionar_navios;
+                        quadradoMarcado[i, j] = false;// Deixei como padrão para todas as Labels False, quando cliacado muda para true
+                        PainelJogo.Controls.Add(quadradoGlobal[i, j]);
+                    }
+                }
+                for (int i = 0; i < 5; i++)
+                {
+                    BotoesNavios[i] = new Button()
+                    {
+                        Location = new Point(0, i * 30),
+                        Name = "button1",
+                        Size = new System.Drawing.Size(196, 30),
+                        TabIndex = 1,
+                        Text = QuantidadeNavios[i],
+                        UseVisualStyleBackColor = true
+                     };
+                    panel1.Controls.Add(BotoesNavios[i]);
+                    
+                }
+            }
+        }
+
+
+
+        private void Selecionar_navios(object sender, System.EventArgs e)
+        {
+            Label lbl = (Label)sender;//Ele chama a Label clickada, pelo menos foi o que entendi
+            Color color = lbl.BackColor;//Pela lb clicada ele extrai os valores para Color e Local
             Point local = lbl.Location;
 
             if (color == System.Drawing.Color.White)
             {
                 color = System.Drawing.Color.Black;
-                quadradoMarcado[lbl.Location.X / 20, lbl.Location.Y / 20] = true;
-                Console.WriteLine(quadradoMarcado[lbl.Location.X / 20, lbl.Location.Y / 20]);
+                quadradoMarcado[lbl.Location.X / 30, lbl.Location.Y / 30] = true;
+                Console.WriteLine(quadradoMarcado[lbl.Location.X / 30, lbl.Location.Y / 30]);
             }
             else if (color == System.Drawing.Color.Black)
             {
-                color = System.Drawing.Color.Red;               
-                quadradoMarcado[lbl.Location.X / 20, lbl.Location.Y / 20] = true;
-                Console.WriteLine(quadradoMarcado[lbl.Location.X / 20, lbl.Location.Y / 20]);
+                color = System.Drawing.Color.Red;
+                Console.WriteLine(quadradoMarcado[lbl.Location.X / 30, lbl.Location.Y / 30]);
             }
             else
             {
                 color = System.Drawing.Color.White;
-                quadradoMarcado[lbl.Location.X / 20, lbl.Location.Y / 20] = false;
-                Console.WriteLine(quadradoMarcado[lbl.Location.X / 20, lbl.Location.Y / 20]);
+                Console.WriteLine(quadradoMarcado[lbl.Location.X / 30, lbl.Location.Y / 30]);
             }
             lbl.BackColor = color;
             Console.WriteLine(lbl.Location);
@@ -53,20 +101,8 @@ namespace BatalhaNavalGrupo3
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
-            for (int i = 0; i < RowCount; i++)
-            {
-                for (int j = 0; j < ColumnCount; j++)
-                {
-                    quadradoGlobal[i, j] = new Label();
-                    quadradoGlobal[i, j].Name = $"({i},{j})";
-                    quadradoGlobal[i, j].Size = new Size(20, 20);
-                    quadradoGlobal[i, j].Location = new Point(i * 20, j * 20);
-                    quadradoGlobal[i, j].BackColor = Color.White;
-                    quadradoGlobal[i, j].Click += selecionar_navios;
-                    quadradoMarcado[i, j] = false;
-                    panel1.Controls.Add(quadradoGlobal[i, j]);
-                }
-            }
+
         }
     }
 }
+
