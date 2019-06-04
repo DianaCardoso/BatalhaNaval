4a215dadb8ac5fec09fb53c684b8bc1779bec8bc
Oscar H. L. Henrichs <oscar.henrichs.work@outlook.com>
2019-06-04 11:32:18 -0300

    Botoes e painel prontos.

--- BatalhaNavalGrupo3/BatalhaNavalGrupo3/Form1.Designer.cs
+++ BatalhaNavalGrupo3/BatalhaNavalGrupo3/Form1.Designer.cs
@@ -29,26 +29,30 @@
         private void InitializeComponent()
         {
             this.PainelJogo = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Botao_Definir_Jogo = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Botao_Comecar_Jogo = new System.Windows.Forms.Button();
+            this.computador_jogo = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // PainelJogo
             // 
-            this.PainelJogo.Location = new System.Drawing.Point(307, 12);
+            this.PainelJogo.BackColor = System.Drawing.SystemColors.Control;
+            this.PainelJogo.Location = new System.Drawing.Point(408, 12);
             this.PainelJogo.Name = "PainelJogo";
-            this.PainelJogo.Size = new System.Drawing.Size(591, 524);
+            this.PainelJogo.Size = new System.Drawing.Size(536, 496);
             this.PainelJogo.TabIndex = 0;
             // 
-            // button1
+            // Botao_Definir_Jogo
             // 
-            this.button1.Location = new System.Drawing.Point(22, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(261, 68);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Começar";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.Botao_Start);
+            this.Botao_Definir_Jogo.Location = new System.Drawing.Point(22, 12);
+            this.Botao_Definir_Jogo.Name = "Botao_Definir_Jogo";
+            this.Botao_Definir_Jogo.Size = new System.Drawing.Size(261, 68);
+            this.Botao_Definir_Jogo.TabIndex = 1;
+            this.Botao_Definir_Jogo.Text = "Definir Jogo";
+            this.Botao_Definir_Jogo.UseVisualStyleBackColor = true;
+            this.Botao_Definir_Jogo.Click += new System.EventHandler(this.Botao_Start);
             // 
             // panel1
             // 
@@ -56,18 +60,46 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(261, 450);
             this.panel1.TabIndex = 2;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(290, 86);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(112, 450);
+            this.panel2.TabIndex = 3;
+            // 
+            // Botao_Comecar_Jogo
+            // 
+            this.Botao_Comecar_Jogo.Location = new System.Drawing.Point(22, 543);
+            this.Botao_Comecar_Jogo.Name = "Botao_Comecar_Jogo";
+            this.Botao_Comecar_Jogo.Size = new System.Drawing.Size(261, 71);
+            this.Botao_Comecar_Jogo.TabIndex = 4;
+            this.Botao_Comecar_Jogo.Text = "Começar";
+            this.Botao_Comecar_Jogo.UseVisualStyleBackColor = true;
+            this.Botao_Comecar_Jogo.Click += new System.EventHandler(this.Botao_Comecar_Jogo_Click);
+            // 
+            // computador_jogo
+            // 
+            this.computador_jogo.BackColor = System.Drawing.SystemColors.Control;
+            this.computador_jogo.Location = new System.Drawing.Point(954, 12);
+            this.computador_jogo.Name = "computador_jogo";
+            this.computador_jogo.Size = new System.Drawing.Size(536, 496);
+            this.computador_jogo.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(910, 549);
+            this.ClientSize = new System.Drawing.Size(1502, 643);
+            this.Controls.Add(this.computador_jogo);
+            this.Controls.Add(this.Botao_Comecar_Jogo);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Botao_Definir_Jogo);
             this.Controls.Add(this.PainelJogo);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -75,8 +107,11 @@
         #endregion
 
         private System.Windows.Forms.Panel PainelJogo;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Botao_Definir_Jogo;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button Botao_Comecar_Jogo;
+        private System.Windows.Forms.Panel computador_jogo;
     }
 }
 
--- BatalhaNavalGrupo3/BatalhaNavalGrupo3/Form1.cs
+++ BatalhaNavalGrupo3/BatalhaNavalGrupo3/Form1.cs
@@ -19,27 +19,35 @@ namespace BatalhaNavalGrupo3
             InitializeComponent();
         }
 
-        // Numero de Colunas e linhas
+        //Labels e Botões gerados dentro dos paineis.
+        Label[,] painel_de_quadrados_do_jogador = new Label[10, 10]; // Armazenei cada label gerada dentro de uma matriz.
+        Label[,] painel_de_quadrados_do_computador = new Label[10, 10]; // Armazenei cada label gerada dentro de uma matriz.
+        Label[] valoresBotoes = new Label[4]; // Armazenei cada label gerada dentro de uma matriz.
+        Button[] BotoesNavios = new Button[5];// Botões gerados com cada Navio.
+
+        // Numero de Colunas, linhas e tamanho dos quadrados dentro de painel.
         const int RowCount = 10;
         const int ColumnCount = 10;
         const int tamanho_quadrados = 40;
+
+        //Booleans que determinam o estado de jogo e informações de jogadas
         Boolean definindo_jogo = false;
         Boolean jogando_jogo = false;
-        int botao_ativo_cinco = 0;
-        int botao_ativo_quatro = 0;
-        int botao_ativo_tres = 0;
-        int botao_ativo_tres_contador = 0;
-        int botao_ativo_dois = 0;
+        bool botao_ativo_tres_contador = false;
+        bool[,] quadradoMarcado = new bool[10, 10]; // Um Boolean pra salvar os quadrados marcados.
+
 
 
         //Valores Armazenados
-        Label[,] quadradoGlobal = new Label[10, 10]; // Armazenei cada label gerada dentro de uma matriz.
-        bool[,] quadradoMarcado = new bool[10, 10]; // Um Boolean pra salvar os quadrados marcados.
-        Button[] BotoesNavios = new Button[5];// Botões gerados com cada Navio.
-        string[] QuantidadeVidasNavios = new string[5] { "5", "4", "3", "3", "2" };//Mensagem de Texto dos Botões.
-        int[] QuantidadePosicoesNavios = new int[5] { 5, 4, 3, 3, 2 };//Valor que cada Botão gerado representa pra posições dos navios.
+        string[] QuantidadeVidasNavios = new string[4] { "5", "4", "3", "2"};//Mensagem de Texto dos Botões.
+        string[] quantidade_de_navios = new string[4] { "1", "1", "2", "1" };//Mensagem de Texto dos Botões.
+        int[] botao_ativo = new int[4] { 1, 1, 1, 1 };
+        int[] QuantidadePosicoesNavios = new int[4] { 5, 4, 3, 2};//Valor que cada Botão gerado representa pra posições dos navios.
         int Botao_Navio_Selecionado = 0;//Recebe o valor do botão selecionado.
+        int posicaoX_do_navio = 0;
+        int posicaoY_do_navio = 0;
 
+        //Botão para iniciar o jogo e gerar os botões com os navios.
         private void Botao_Start(object sender, EventArgs e)
         {
             definindo_jogo = true;
@@ -49,33 +57,47 @@ namespace BatalhaNavalGrupo3
                 {
                     for (int j = 0; j < ColumnCount; j++)
                     {
-                        if (button1.Text == "Recomeçar")
+                        if (Botao_Definir_Jogo.Text == "Redefinir/Recomeçar")
                         {
-                            quadradoGlobal[i, j].Name = $"({i},{j})";
-                            quadradoGlobal[i, j].BackColor = Color.White;
+                            painel_de_quadrados_do_jogador[i, j].Name = $"({i},{j})";
+                            painel_de_quadrados_do_jogador[i, j].BackColor = Color.Aqua;
                             quadradoMarcado[i, j] = false;
                             Botao_Navio_Selecionado = 0;
-                            botao_ativo_cinco = 0;
-                            botao_ativo_quatro = 0;
-                            botao_ativo_tres = 0;
-                            botao_ativo_dois = 0;
+                            valoresBotoes[0].Text = "1";
+                            valoresBotoes[1].Text = "1";
+                            valoresBotoes[2].Text = "2";
+                            valoresBotoes[3].Text = "1";
+                            botao_ativo[0] = 1;
+                            botao_ativo[1] = 1;
+                            botao_ativo[2] = 1;
+                            botao_ativo[3] = 1;
+                            botao_ativo_tres_contador = false;
                         }
                         else
                         {
-                            quadradoGlobal[i, j] = new Label
+                            painel_de_quadrados_do_jogador[i, j] = new Label
                             {
                                 Name = $"({i},{j})",
                                 Size = new Size(tamanho_quadrados, tamanho_quadrados),
                                 Location = new Point(i * tamanho_quadrados, j * tamanho_quadrados),
-                                BackColor = Color.White
+                                BackColor = Color.Aqua
                             };
-                            quadradoGlobal[i, j].Click += Selecionar_navios;
-                            quadradoMarcado[i, j] = false;// Deixei como padrão para todas as Labels False, quando cliacado muda para true
-                            PainelJogo.Controls.Add(quadradoGlobal[i, j]);
+
+                            painel_de_quadrados_do_computador[i, j] = new Label
+                            {
+                                Name = $"({i},{j})",
+                                Size = new Size(tamanho_quadrados, tamanho_quadrados),
+                                Location = new Point(i * tamanho_quadrados, j * tamanho_quadrados),
+                                BackColor = Color.Aqua
+                            };
+                            painel_de_quadrados_do_jogador[i, j].Click += Selecionar_navios; // Adiciona o painel de quadrados para o jagador.
+                            quadradoMarcado[i, j] = false;// Deixei como padrão para todas as Labels False, quando cliacado muda para true, identifica os quadrados marcados
+                            PainelJogo.Controls.Add(painel_de_quadrados_do_jogador[i, j]); // Adiciona o segundo painel de quadrados no Form 1 onde ficaram os navios do COM.
+                            computador_jogo.Controls.Add(painel_de_quadrados_do_computador[i, j]);
                         }
                     }
                 }
-                for (int i = 0; i < 5; i++)
+                for (int i = 0; i < 4; i++)
                 {
                     BotoesNavios[i] = new Button()
                     {
@@ -85,80 +107,175 @@ namespace BatalhaNavalGrupo3
                         TabIndex = 1,
                         Text = QuantidadeVidasNavios[i],
                         UseVisualStyleBackColor = true
-                     };
+                    };
                     panel1.Controls.Add(BotoesNavios[i]);
                     BotoesNavios[i].Click += Botao_Navios;
+
+                    valoresBotoes[i] = new Label()
+                    {
+                        Name = $"(label {i})",
+                        Size = new Size(25, 30),
+                        Location = new Point(0, 20 + (i * 60)),
+                        BackColor = Color.White,
+                        Text = quantidade_de_navios[i],
+                        Font = new Font ("Arial", 16, FontStyle.Bold)
+                    };
+                    panel2.Controls.Add(valoresBotoes[i]);
                 }
-                button1.Text = "Recomeçar";
+                Botao_Definir_Jogo.Text = "Redefinir/Recomeçar";
             }
         }
 
+        //Para cada botão de navio eles retornam um valor x.
         private void Botao_Navios(object sender, EventArgs e)
         {
             Button clicarBotao = (Button)sender;
             Botao_Navio_Selecionado = QuantidadePosicoesNavios[clicarBotao.Location.Y / 60];
         }
 
+        //Aqui é da onde são tirados os valores de cada quadrado da tabala do jogo. 
         private void Selecionar_navios(object sender, System.EventArgs e)
         {
             Label lbl = (Label)sender;//Ele chama a Label clicada.
             Color color = lbl.BackColor;//Pela lb clicada ele extrai os valores para Color e Local
             Point local = lbl.Location;
-            //Pelo botão selecionado ele escolhe qual if seguir, ainda não limitei o numero de casas que são possiveis marcar
+            //Pelo botão selecionado ele escolhe qual if seguir, falta limitar as direções possiveis.
             if (definindo_jogo == true)
             {
                 if (Botao_Navio_Selecionado == 5)
                 {
-                    if (color == System.Drawing.Color.White & botao_ativo_cinco < 5)
+                    if (color == System.Drawing.Color.Aqua & botao_ativo[0] <= 5)
                     {
-                        color = System.Drawing.Color.Black;
-                        quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados] = true;
-                        botao_ativo_cinco += 1;
-                        Console.WriteLine(quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados]);
+                        if (botao_ativo[0] == 1)
+                        {
+                            color = System.Drawing.Color.Black;
+                            quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados] = true; // Pela localização X e Y divididos pelo tamanho do quadrado chego no i e j.
+                            botao_ativo[0] += 1;
+                            posicaoX_do_navio = lbl.Location.X;
+                            posicaoY_do_navio = lbl.Location.Y;
+                            Console.WriteLine(posicaoX_do_navio);
+                        }
+                        else if ((posicaoX_do_navio + tamanho_quadrados * (botao_ativo[0] - 1) == lbl.Location.X || posicaoX_do_navio - tamanho_quadrados * (botao_ativo[0] - 1) == lbl.Location.X) & posicaoY_do_navio == lbl.Location.Y)
+                        {
+                            color = System.Drawing.Color.Black;
+                            quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados] = true; // Pela localização X e Y divididos pelo tamanho do quadrado chego no i e j.
+                            botao_ativo[0] += 1;
+                        }
+                        else if ((posicaoY_do_navio + tamanho_quadrados * (botao_ativo[0] - 1) == lbl.Location.Y || posicaoY_do_navio - tamanho_quadrados * (botao_ativo[0] - 1) == lbl.Location.Y) & posicaoX_do_navio == lbl.Location.X)
+                        {
+                            color = System.Drawing.Color.Black;
+                            quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados] = true; // Pela localização X e Y divididos pelo tamanho do quadrado chego no i e j.
+                            botao_ativo[0] += 1;
+                        }
+                    }
+                    if (botao_ativo[0] == 6)
+                    {
+                        valoresBotoes[0].Text = "0";
+                        posicaoX_do_navio = 0;
+                        posicaoY_do_navio = 0;
                     }
                     lbl.BackColor = color;
                     Console.WriteLine(lbl.Location);
                 }
-                else if (Botao_Navio_Selecionado == 4 & botao_ativo_quatro < 4)
+                else if (Botao_Navio_Selecionado == 4)
                 {
-                    if (color == System.Drawing.Color.White)
+                    if (color == System.Drawing.Color.Aqua & botao_ativo[1] <= 4)
                     {
-                        color = System.Drawing.Color.Black;
-                        quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados] = true;
-                        botao_ativo_quatro += 1;
-                        Console.WriteLine(quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados]);
+                        if (botao_ativo[1] == 1)
+                        {
+                            color = System.Drawing.Color.Black;
+                            quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados] = true; // Pela localização X e Y divididos pelo tamanho do quadrado chego no i e j.
+                            botao_ativo[1] += 1;
+                            posicaoX_do_navio = lbl.Location.X;
+                            posicaoY_do_navio = lbl.Location.Y;
+                            Console.WriteLine(posicaoX_do_navio);
+                        }
+                        else if ((posicaoX_do_navio + tamanho_quadrados * (botao_ativo[1] - 1) == lbl.Location.X || posicaoX_do_navio - tamanho_quadrados * (botao_ativo[1] - 1) == lbl.Location.X) & posicaoY_do_navio == lbl.Location.Y)
+                        {
+                            color = System.Drawing.Color.Black;
+                            quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados] = true; // Pela localização X e Y divididos pelo tamanho do quadrado chego no i e j.
+                            botao_ativo[1] += 1;
+                        }
+                        else if ((posicaoY_do_navio + tamanho_quadrados * (botao_ativo[1] - 1) == lbl.Location.Y || posicaoY_do_navio - tamanho_quadrados * (botao_ativo[1] - 1) == lbl.Location.Y) & posicaoX_do_navio == lbl.Location.X)
+                        {
+                            color = System.Drawing.Color.Black;
+                            quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados] = true; // Pela localização X e Y divididos pelo tamanho do quadrado chego no i e j.
+                            botao_ativo[1] += 1;
+                        }
+                    }
+                    if (botao_ativo[1] == 5)
+                    {
+                        valoresBotoes[1].Text = "0";
                     }
                     lbl.BackColor = color;
                     Console.WriteLine(lbl.Location);
                 }
-                else if (Botao_Navio_Selecionado == 3 & botao_ativo_tres < 3)
+                else if (Botao_Navio_Selecionado == 3 & botao_ativo[2] <= 3 & botao_ativo_tres_contador == false)
                 {
-                    if (color == System.Drawing.Color.White)
+                    if (color == System.Drawing.Color.Aqua)
                     {
                         color = System.Drawing.Color.Black;
                         quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados] = true;
-                        botao_ativo_tres += 1;
-                        Console.WriteLine(quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados]);
+                        botao_ativo[2] += 1;
+                    }
+                    if (botao_ativo[2] == 4)
+                    {
+                        botao_ativo_tres_contador = true;
+                        botao_ativo[2] = 1;
+                        valoresBotoes[2].Text = "1";
                     }
                     lbl.BackColor = color;
                     Console.WriteLine(lbl.Location);
                 }
-                else if (Botao_Navio_Selecionado == 2 & botao_ativo_dois < 2)
+                else if (Botao_Navio_Selecionado == 3 & botao_ativo[2] <= 3 & botao_ativo_tres_contador == true)
                 {
-                    if (color == System.Drawing.Color.White)
+                    if (color == System.Drawing.Color.Aqua)
                     {
                         color = System.Drawing.Color.Black;
                         quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados] = true;
-                        botao_ativo_dois += 1;
-                        Console.WriteLine(quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados]);
+                        botao_ativo[2] += 1;
+                    }
+                    if (botao_ativo[2] == 4)
+                    {
+                        valoresBotoes[2].Text = "0";
                     }
                     lbl.BackColor = color;
                     Console.WriteLine(lbl.Location);
                 }
+                else if (Botao_Navio_Selecionado == 2 & botao_ativo[3] <= 2)
+                {
+                    if (color == System.Drawing.Color.Aqua)
+                    {
+                        color = System.Drawing.Color.Black;
+                        quadradoMarcado[lbl.Location.X / tamanho_quadrados, lbl.Location.Y / tamanho_quadrados] = true;
+                        botao_ativo[3] += 1;
+                    }
+                    if (botao_ativo[3] == 3)
+                    {
+                        valoresBotoes[3].Text = "0";
+                    }
+                    lbl.BackColor = color;
+                    Console.WriteLine(lbl.Location);
+                }
+            }            
+        }
+
+        private void Botao_Comecar_Jogo_Click(object sender, EventArgs e)
+        {
+            if (botao_ativo[0] == 6 & botao_ativo[1] == 5 & botao_ativo[2] == 4 & botao_ativo[3] == 3 & botao_ativo_tres_contador == true)
+            {
+                for (int i = 0; i < RowCount; i++)// Esses Laços for geram a tabela de labels.
+                {
+                    for (int j = 0; j < ColumnCount; j++)
+                    {
+                        painel_de_quadrados_do_jogador[i, j].BackColor = Color.White;
+                    }
+                }
+                definindo_jogo = false;
             }
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
 
         }
